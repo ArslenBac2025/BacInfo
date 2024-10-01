c49eef2ed7dc7083f14d0129b2c43627bce12a41
4Info1 <zenos.zenos001@gmail.com>
2024-10-01 15:22:11 +0200

    Update binaire.py

--- binaire.py
+++ binaire.py
@@ -1,7 +1,6 @@
 def binaire(entier, base):
     resultat = ""
     hexN = "0123456789ABCDEF"
-    
     while entier > 0:
         rest = entier % base
         if base == 16:
@@ -9,7 +8,6 @@ def binaire(entier, base):
         else:
             resultat = str(rest) + resultat 
         entier = entier // base
-    
     return resultat 
 
 print(binaire(12054, 8))
